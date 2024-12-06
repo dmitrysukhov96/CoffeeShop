40fc4643ef3e26ebfa3e8b4e147f5a8a09c9c6cf
dima <dmitrysukhov96@gmail.com>
2024-12-06 22:10:11 +0200

    test

--- app/src/main/java/com/dmitrysukhov/coffeeshop/MainActivity.kt
+++ app/src/main/java/com/dmitrysukhov/coffeeshop/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.dmitrysukhov.coffeeshop.ui.theme.CoffeeShopTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello, $name!",
-        modifier = modifier
+        modifier = modifier, fontSize = 20.sp
     )
 }
 
