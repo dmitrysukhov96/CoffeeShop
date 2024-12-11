5b3b73f8fa4c99abb7a67c256f6623ad4202eb64
Timofiy <sulimovtimofej41@gmail.com>
2024-12-11 13:40:18 +0200

    добавил кнопку

--- app/src/main/java/com/dmitrysukhov/coffeeshop/DetailsScreen.kt
+++ app/src/main/java/com/dmitrysukhov/coffeeshop/DetailsScreen.kt
@@ -1,12 +1,37 @@
-  package com.dmitrysukhov.coffeeshop
+package com.dmitrysukhov.coffeeshop
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-
-fun DetailsScreen () {
-
-    Text(text = "DetailsScreen")
-
+fun DetailsScreen(onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(240.dp)
+            .height(60.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFD17842),
+            contentColor = Color.White
+        ),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Text(
+            text = "Add to Cart",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+    }
 }
--- app/src/main/java/com/dmitrysukhov/coffeeshop/MainActivity.kt
+++ app/src/main/java/com/dmitrysukhov/coffeeshop/MainActivity.kt
@@ -1,29 +1,23 @@
- package com.dmitrysukhov.coffeeshop
+package com.dmitrysukhov.coffeeshop
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.dmitrysukhov.coffeeshop.ui.theme.CoffeeShopTheme
 
-class MainActivity : ComponentActivity() {
+ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CoffeeShopTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "CoffeeCode Devs",
-                        modifier = Modifier.padding(innerPadding)
+                    DetailsScreen ({}
+
                     )
                 }
             }
@@ -31,21 +25,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello, $name!",
-        modifier = modifier, fontSize = 20.sp
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoffeeShopTheme {
-        Greeting("Android")
-    }
-}
 
 //Timofey
 
