0a4ed74f7a2f06ca2590dad90259ad1e1d611bdd
dima <dmitrysukhov96@gmail.com>
2024-12-11 16:21:21 +0200

    добавил шрифт

--- app/src/main/java/com/dmitrysukhov/coffeeshop/DetailsScreen.kt
+++ app/src/main/java/com/dmitrysukhov/coffeeshop/DetailsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -17,21 +18,16 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun DetailsScreen(onClick: () -> Unit) {
     Button(
-        onClick = onClick,
-        modifier = Modifier
+        onClick = onClick, modifier = Modifier
             .width(240.dp)
-            .height(60.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color(0xFFD17842),
-            contentColor = Color.White
-        ),
-        shape = RoundedCornerShape(16.dp)
+            .height(60.dp), colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFFD17842), contentColor = Color.White
+        ), shape = RoundedCornerShape(16.dp)
     ) {
         Text(
-            text = "Add to Cart",
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
+            text = "Add to Cart", fontSize = 16.sp, fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            style = TextStyle(fontFamily = poppinsFontFamily, fontWeight = FontWeight.SemiBold)
         )
     }
 }
---
+++ app/src/main/java/com/dmitrysukhov/coffeeshop/FontFamily.kt
@@ -0,0 +1,10 @@
+package com.dmitrysukhov.coffeeshop
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val poppinsFontFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold)
+)
