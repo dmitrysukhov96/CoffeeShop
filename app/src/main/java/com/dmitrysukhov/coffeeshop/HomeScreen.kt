abe4369ba1da07602cae8f567da4512b2276a5ab
dima <dmitrysukhov96@gmail.com>
2025-01-14 21:36:47 +0200

    textfield

--- app/src/main/java/com/dmitrysukhov/coffeeshop/HomeScreen.kt
+++ app/src/main/java/com/dmitrysukhov/coffeeshop/HomeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -19,7 +20,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -50,7 +56,10 @@ fun HomeScreen() {
             fontWeight = FontWeight.W600,
             fontFamily = poppinsFontFamily
         )
-        Spacer(Modifier.height(60.dp))
+        var searchQuery by rememberSaveable { mutableStateOf("") }
+        Row(Modifier.padding(horizontal = 30.dp, vertical = 28.dp)) {
+            TextField(searchQuery, { searchQuery = it })
+        }
         LazyRow {
             items(coffeeList) { coffee ->
                 ListItem(coffee, {
