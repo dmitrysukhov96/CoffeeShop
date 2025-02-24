26a642b6e7623490e20b0e308919085dcb3713f4
Skover3563 <kostiaostrouhov6e@gmail.com>
2025-02-25 00:09:16 +0200

    Cделал все карточки в CartScreen

--- app/src/main/java/com/dmitrysukhov/coffeeshop/FavoritesScreen.kt
+++ app/src/main/java/com/dmitrysukhov/coffeeshop/FavoritesScreen.kt
@@ -2,13 +2,24 @@ package com.dmitrysukhov.coffeeshop
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FavouritesScreen() {
@@ -19,9 +30,26 @@ fun FavouritesScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     )
-    {
-        Text(text = "FavouritesScreen", color = Color.White)
+    {}
+        LazyColumn (
+            modifier = Modifier
+            .width(330.dp)
+            .height(255.dp)
+            .clip(RoundedCornerShape(23.dp))
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(DarkGrey2, DarkGrey2.copy(0.0f)),
+                    start = Offset(0f, 0f), end = Offset(1000f, 1000f)
+                )
+            )
+            .padding(12.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ){
+        }
     }
-}
+
+
 
 const val FAVORITES_SCREEN = "FavoritesScreen"
